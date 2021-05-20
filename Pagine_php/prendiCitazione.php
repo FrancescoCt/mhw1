beaa6d7de446e204b96dcbfb3d7a716c65c6c470
Francesco Catania <81304650+FrancescoCt@users.noreply.github.com>
2021-05-20 14:22:43 +0200

    Delete Pagine_php directory

--- Pagine_php/prendiCitazione.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	header('Content-Type: application/json');
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL,"https://api.quotable.io/random");
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	$result = curl_exec($curl);
-	curl_close($curl);
-	echo $result;
-?>
